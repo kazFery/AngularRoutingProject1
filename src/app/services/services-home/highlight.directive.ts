47978fc6ddefc6f6d9871061df7da3f8bd719723
Farzaneh Kazemi <kazem.farzan@gmail.com>
2023-04-04 10:01:12 -0400

    adding dirctive to service

--- src/app/services/services-home/highlight.directive.ts
+++ src/app/services/services-home/highlight.directive.ts
@@ -1,12 +1,10 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
 export class HighlightDirective {
-
-  constructor(private e1 : ElementRef ) {
-     e1.nativeElement.style.backgroundColor = '#E23D28';
-   }
-
+  constructor(private e1: ElementRef) {
+    e1.nativeElement.style.backgroundColor = '#E23D28';
+  }
 }
--- src/app/services/services-home/services-home.component.ts
+++ src/app/services/services-home/services-home.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services-home',
   templateUrl: './services-home.component.html',
-  styleUrls: ['./services-home.component.css']
+  styleUrls: ['./services-home.component.css'],
 })
-export class ServicesHomeComponent {
-
+export class ServicesHomeComponent implements OnInit {
+  ngOnInit(): void {
+      
+  }
+  myServices = ['Consulting', 'Business Analysis', 'Customer service', 'Design Database'];
 }
